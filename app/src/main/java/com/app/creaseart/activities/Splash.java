9dd5ed0bbe47d4133ebd631a3852be9a7f6f500e
hemanta <hemantagarg5@gmail.com>
2017-09-10 17:58:17 +0530

    First commit

---
+++ app/src/main/java/com/app/creaseart/activities/Splash.java
@@ -0,0 +1,18 @@
+package com.app.creaseart.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.app.creaseart.R;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+
+    }
+}
---
+++ app/src/main/java/com/app/creaseart/interfaces/JsonApiHelper.java
@@ -0,0 +1,30 @@
+package com.app.creaseart.interfaces;
+
+/**
+ * Created by hemanta on 29-07-2017.
+ */
+
+public interface JsonApiHelper {
+
+    String BASEURL = "http://sfscoring.betasportzfever.com/";
+    String LOGIN = "sflogin";
+    String GET_FEEDS = "getFeeds/";
+    String GET_NOTIFICATION = "getNotifications/";
+    String GET_FRIENDREQUEST = "getFriendRequests/";
+    String GET_TEAMJOINREQUEST = "getAllTeamInvitation/";
+    String GET_MATCHINVITATIONAVAILABILITY = "getMatchAndPrctiseInvitations/";
+    String GET_UPCOMINGEVENTS = "getUpcomingEvent/";
+    String GET_COMMENTS = "getComments/";
+    String GET_LIKES = "getLikes/";
+    String GET_SHARE = "getShares/";
+    String GET_FRIENDLIST = "getFriendsList/";
+    String GET_RECENTCHAT = "getRecentChat/";
+    String SHAREFEED = "shareFeed";
+    String POSTCOMMENT = "comment";
+    String UPDATECOMMENT = "updateComment";
+    String DELETECOMMENT = "deleteComment/";
+
+    String SEND_MESSAGE = "";
+    String GET_MESSAGE_LIST="";
+    String GET_CHAT_LIST="";
+}
